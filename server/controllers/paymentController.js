6a65f051839b7b076271fe5eec890105360a692e
ejmichael <ethanj.michael03@gmail.com>
2026-04-30 22:14:46 +0200

    Added fix for Payfast decline of payment

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -40,7 +40,7 @@ export const createSubscriptionCheckout = async (req, res) => {
     let initialAmount = '149.00';
     if (business.subscriptionStatus === 'pending_setup') {
       billingDate.setDate(billingDate.getDate() + 30);
-      initialAmount = '23.00'; // Non-round amount to help bypass small-transaction fraud filters
+      initialAmount = '10.00';
     }
     const billingDateString = billingDate.toISOString().split('T')[0];
 
